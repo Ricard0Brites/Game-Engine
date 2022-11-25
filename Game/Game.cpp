65800040c99c6ff1f54116c57fe2d50700da07b9
Ricardo Brites <ricardo.brites.work@gmail.com>
2022-11-25 10:39:46 +0000

    Removed generated code created by git

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -17,7 +17,6 @@ int main(int argc, char ** argv)
     TestActor->AssignTexture("F:/Year3/Game-Engine/Engine/Temp/graphics/drone.bmp", 8, 2, 2);
     TestActor->GetSpriteComponent()->PlayAnimation(true);
 
-<<<<<<< Updated upstream
     Actor* TestActor2 = engine.CreateActor<Actor>(nullptr);
     TestActor2->AssignTexture("F:/Year3/Game-Engine/Engine/Temp/graphics/drone.bmp", 8, 2, 2);
     TestActor2->GetSpriteComponent()->SetLocation(Vector::CreateVector(100, 100, 0), nullptr);
@@ -27,9 +26,6 @@ int main(int argc, char ** argv)
     TestActor3->AssignTexture("F:/Year3/Game-Engine/Engine/Temp/graphics/drone.bmp", 8, 2, 2);
     TestActor3->GetSpriteComponent()->SetLocation(Vector::CreateVector(200, 100, 0), nullptr);
     TestActor3->GetSpriteComponent()->PlayAnimation(true);
-
-=======
->>>>>>> Stashed changes
 
     engine.start();// no code after this function will be called. from this point on to do anything it has to be inside the gameloop... this means only spawned and active actors can spawn other actors.
     
